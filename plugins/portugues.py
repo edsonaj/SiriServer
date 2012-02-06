e9b760f21b37f2c765dcbcd865a5b7673cae688b
Edson Alves Junior <edson.junior@me.com>
2012-02-06 18:28:02 -0200

    portugues plugin fixed

--- plugins/portugues.py
+++ plugins/portugues.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #Siri Server plugin - Portugues
 
 from plugin import *
@@ -5,17 +6,17 @@ from plugin import *
 class portugues(Plugin):
     
 
-    @register("en-US", "(.*Ola.*)|(.*Ola.*Siri.*)|(.*Oi.*)|(.*Oi.*Siri.*)")
+    @register("en-US", "(.*Olá.*)|(.*Olá.*Siri.*)|(.*Oi.*)|(.*Oi.*Siri.*)")
     def st_hello(self, speech, language):
-        self.say("Ola","Hello")
+        self.say("Olá","Hello")
         self.complete_request()
 
     @register("en-US", "(.*seu nome.*)")
     def st_name(self, speech, language):
-        self.say("Meu nome e Siri.","My name is Siri.")
+        self.say("Meu nome é Siri.","My name is Siri.")
         self.complete_request()
     
-    @register("en-US", "(.*Como.*voce.*esta.*)")
+    @register("en-US", "(.*Como.*você.*está.*)")
     def st_howareyou(self, speech, language):
         self.say("Muito bem, obrigada por perguntar!","Fine, thanks for asking!")
         self.complete_request()
@@ -23,12 +24,12 @@ class portugues(Plugin):
     @register("en-US", ".*Obrigado.*")
     def st_thank_you(self, speech, language):
         self.say("De nada.","You are welcome.")
-        self.say("E apenas meu trabalho.","This is my job.")
+        self.say("É apenas meu trabalho.","This is my job.")
         self.complete_request()     
     
     @register("en-US", "(.*quer.*casar.*)|(.*vamos.*casar.*)")
     def st_marry_me(self, speech, language):
-        self.say("NAo, obrigada. Eu estou apaixonada pelo iPhone preto do seu amigo.","No, thank you. I'm in love with the black iPhone from your friend.")
+        self.say("Não, obrigada. Eu estou apaixonada pelo iPhone preto do seu amigo.","No, thank you. I'm in love with the black iPhone from your friend.")
         self.complete_request()
     
     @register("en-US", "(.*conte.*piada*)|(.*contar.*piada*)")
@@ -38,10 +39,10 @@ class portugues(Plugin):
 
     @register("en-US", "(.*conte.*historia*)|(.*contar.*historia*)")
     def st_tell_story(self, speech, language):
-        self.say("Muito, mas muito distante, existia ... nao, muito estupida.","Far far away, there was ... no, too stupid")
+        self.say("Muito, mas muito distante, existia ... não, muito estupida esta história.","Far far away, there was ... no, too stupid")
         self.complete_request()
     
     @register("en-US", ".*significado.*vida*")
     def st_tell_story(self, speech, language):
-        self.say("Eu nao sei, mas deve ter algum app para isso.","I don't know, but it should have an app for that")
+        self.say("Eu não sei, mas deve ter algum app para isso.","I don't know, but it should have an app for that")
         self.complete_request()
