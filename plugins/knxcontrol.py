116f363206eaf41286a0a2f37a0c5e2c20d7c1d8
Edson Alves Junior <edson.junior@me.com>
2012-02-06 20:39:05 -0200

    KNX edit

--- plugins/knxcontrol.py
+++ plugins/knxcontrol.py
@@ -8,25 +8,56 @@ class knxcontrol(Plugin):
     
 ##OFICE
     
-    @register("pt-BR", "(.*escritório.*ligar.*)|(.*ligar.*escritório.*)|(.*escritorio.*ligue.*)|(.*ligue.*escritorio.*)")
+    @register("pt-BR", "(.*escritório.*ligar.*)|(.*ligar.*escritório.*)|(.*escritorio.*ligue.*)|(.*ligue.*escritorio.*))|(.*ligar.*estudos.*)|(.*ligue.*estudos.*)")
     def st_officeon(self, speech, language):
         self.say ("Ligando as luzes do escritorio.","Turning on the office lights") 
     	os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_ON.jar'")
     	self.complete_request()
   
-    @register("pt-BR", "(.*escritorio.*desligar.*)|(.*desligar.*escritorio.*)|(.*escritorio.*desligue.*)|(.*desligue.*escritorio.*)")
+    @register("pt-BR", "(.*escritorio.*desligar.*)|(.*desligar.*escritorio.*)|(.*escritorio.*desligue.*)|(.*desligue.*escritorio.*)|(.*desligar.*estudos.*)|(.*desligue.*estudos.*)")
     def st_officeoff(self, speech, language):
     	self.say ("Desligando as luzes do escritorio.","Turning off the office lights") 
     	os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_OFF.jar'")
     	self.complete_request()
+
+##ROOM
+
+    @register("pt-BR", "(.*sala.*ligar.*)|(.*ligar.*sala.*)|(.*sala.*ligue.*)|(.*ligue.*sala.*)")
+    def st_roomon(self, speech, language):
+        response = self.ask ("Deseja ligar a iluminação principal/direta ou a secundária/indireta?","Wich one?")
+        if(response = "(.*Principal.*)|(.*Direta.*)") 
+            self.say ("Ligando as luzes principais da sala.","Turning on living room principal lights") 
+            os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_1.jar'")
+        else
+            if(response = "{.*Secundária.*)|(.*Indireta.*)")
+            self.say ("Ligando as luzes secundárias da sala.","Turning on living room secundary lights")
+            os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_2.jar'")
+            os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_3.jar'")
+        else self.say ("Desculpe-me, eu nao entendi.","Sorry, I didn't get that.")
+        self.complete_request()
+
+    @register("pt-BR", "(.*sala.*desligar.*)|(.*desligar.*sala.*)|(.*sala.*desligue.*)|(.*desligue.*sala.*)")
+    def st_roomon(self, speech, language):
+        response = self.ask ("Deseja desligar a iluminação principal/direta ou a secundária/indireta?","Wich one?")
+        if(response = "(.*Principal.*)|(.*Direta.*)") 
+            self.say ("Desligando as luzes principais da sala.","Turning off living room principal lights") 
+            os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_1.jar'")
+        else
+            if(response = "{.*Secundária.*)|(.*Indireta.*)")
+                self.say ("Desligando as luzes secundárias da sala.","Turning on living room secundary lights")
+                os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_2.jar'")
+                os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_3.jar'")
+        else self.say ("Desculpe-me, eu nao entendi.","Sorry, I didn't get that.")
+        self.complete_request()
+
 
 ##KITCHEN
     
     @register("pt-BR", "(.*cozinha.*ligar.*)|(.*ligar.*cozinha.*)|(.*cozinha.*ligue.*)|(.*ligue.*cozinha.*)")
     def st_kitchenon(self, speech, language):
         self.say ("Ligando as luzes da cozinha.","Turning on kitchen lights") 
-        system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_1.jar'")
-        self.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_2.jar'")
+        os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_1.jar'")
+        os.system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_2.jar'")
         self.complete_request()
     
     @register("pt-BR", "(.*cozinha.*desligar.*)|(.*desligar.*cozinha.*)|(.*cozinha.*desligue.*)|(.*desligue.*cozinha.*)")
