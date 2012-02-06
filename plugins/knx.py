b1c8d38adbb99656462a302ced090c31f01584b5
Edson Alves Junior <edson.junior@me.com>
2012-02-06 17:06:29 -0200

    Errors fix

--- plugins/knx.py
+++ plugins/knx.py
@@ -11,13 +11,13 @@ class knx(Plugin):
     @register("en-US", "(.*escritório.*ligar.*)|(.*ligar.*escritório.*)")
         def st_hello(self, speech, language):
         self.say ("Ligando as luzes do escritório.","Turning on the office lights") 
-    	system "java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_ON.jar'"
+    	system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_ON.jar'")
     	self.complete_request()
   
     @register("en-US", "(.*escritório.*desligar.*)|(.*desligar.*escritório.*)")
         def st_hello(self, speech, language):
     	self.say ("Desligando as luzes do escritório.","Turning off the office lights") 
-    	system "java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_OFF.jar'"
+    	system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_OFF.jar'")
     	self.complete_request()
 
 ##KITCHEN
@@ -25,13 +25,13 @@ class knx(Plugin):
     @register("en-US", "(.*cozinha.*ligar.*)|(.*ligar.*cozinha.*)")
         def st_hello(self, speech, language):
         self.say ("Ligando as luzes da cozinha.","Turning on kitchen lights") 
-        system "java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_1.jar'"
-        system "java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_2.jar'"
+        system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_1.jar'")
+        system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_2.jar'")
         self.complete_request()
     
     @register("en-US", "(.*cozinha.*desligar.*)|(.*desligar.*cozinha.*)")
         def st_hello(self, speech, language):
-        self.say "Turning off kitchen lights..." 
-        system "java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_1.jar'"
-        system "java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_2.jar'"
+        self.say ("Desligando as luzes da cozinha.","Turning off kitchen lights")
+        system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_1.jar'")
+        system ("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_2.jar'")
         self.complete_request()
