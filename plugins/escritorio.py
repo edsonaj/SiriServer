7c0feb9a813a239fd2fc9ebe4a0ff5ba8d5b4261
Edson Alves Junior <edson.junior@me.com>
2012-02-07 20:42:06 -0200

    Error fix

--- plugins/escritorio.py
+++ plugins/escritorio.py
@@ -6,13 +6,13 @@ from plugin import *
 class escritoriocontrol(Plugin):
     
 @register("pt-BR", ".*ligar escritório.*")
-def st_kitchenon(self, speech, language):
+def st_office_on(self, speech, language):
     self.say("Ligando as luzes do escritório.","Turning on office lights") 
     os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_ON.jar'")
     self.complete_request()
 
 @register("pt-BR", "(.*desligar escritório.*)")
-def st_kitchenoff(self, speech, language):
+def st_office_off(self, speech, language):
     self.say("Desligando as luzes do escritório.","Turning off office lights")
     os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_OFF.jar'")
     self.complete_request()
