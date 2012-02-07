37504ed41c337f9cc1b9110bca618d7c73bdfbb1
Edson Alves Junior <edson.junior@me.com>
2012-02-07 17:50:53 -0200

    Test

--- plugins/automacao.py
+++ plugins/automacao.py
@@ -19,19 +19,3 @@ def st_kitchenoff(self, speech, language):
     os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_1.jar'")
     os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_2.jar'")
     self.complete_request()
-
-
-
-
-
-@register("pt-BR", "(.*escritório ligar.*)|(.*ligar escritório.*)|(.*ligue o escritório.*)")
-def st_officeon(self, speech, language):
-    self.say("Ligando as luzes do escritório.","Turning on office lights") 
-    os.system("java -jar '/home/siriproxy/KNX/_Office__Turn_Light_ON.jar'")
-    self.complete_request()
-
-@register("pt-BR", "(.*escritório desligar.*)|(.*desligar escritório.*)|(.*desligue o escritório.*)")
-def st_officeoff(self, speech, language):
-    self.say("Desligando as luzes do escritório.","Turning off office lights")
-    os.system("java -jar '/home/siriproxy/KNX/_Office__Turn_Light_OFF.jar'")
-    self.complete_request()
