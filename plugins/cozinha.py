2b5214f4e2f677925ea0e79c0636b876cad10d51
Edson Alves Junior <edson.junior@me.com>
2012-02-07 21:01:55 -0200

    Plugin Error Fix

--- plugins/cozinha.py
+++ plugins/cozinha.py
@@ -6,16 +6,16 @@ from plugin import *
 
 class cozinhacontrol(Plugin):
     
-@register("pt-BR", ".*ligar cozinha.*")
-def st_kitchenon(self, speech, language):
-    self.say("Ligando as luzes da cozinha.","Turning on kitchen lights") 
-    os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_1.jar'")
-    os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_2.jar'")
-    self.complete_request()
+    @register("pt-BR", ".*ligar cozinha.*")
+    def st_kitchenon(self, speech, language):
+        self.say("Ligando as luzes da cozinha.","Turning on kitchen lights") 
+        os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_1.jar'")
+        os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_ON_2.jar'")
+        self.complete_request()
 
-@register("pt-BR", "(.*desligar cozinha.*)")
-def st_kitchenoff(self, speech, language):
-    self.say("Desligando as luzes da cozinha.","Turning off kitchen lights")
-    os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_1.jar'")
-    os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_2.jar'")
-    self.complete_request()
+    @register("pt-BR", "(.*desligar cozinha.*)")
+    def st_kitchenoff(self, speech, language):
+        self.say("Desligando as luzes da cozinha.","Turning off kitchen lights")
+        os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_1.jar'")
+        os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Kitchen__Turn_Light_OFF_2.jar'")
+        self.complete_request()
--- plugins/escritorio.py
+++ plugins/escritorio.py
@@ -6,14 +6,14 @@ from plugin import *
 
 class escritoriocontrol(Plugin):
     
-@register("pt-BR", ".*ligar escritório.*")
-def st_office_on(self, speech, language):
-    self.say("Ligando as luzes do escritório.","Turning on office lights") 
-    os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_ON.jar'")
-    self.complete_request()
+    @register("pt-BR", ".*ligar escritório.*")
+    def st_office_on(self, speech, language):
+        self.say("Ligando as luzes do escritório.","Turning on office lights") 
+        os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_ON.jar'")
+        self.complete_request()
 
-@register("pt-BR", "(.*desligar escritório.*)")
-def st_office_off(self, speech, language):
-    self.say("Desligando as luzes do escritório.","Turning off office lights")
-    os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_OFF.jar'")
-    self.complete_request()
+    @register("pt-BR", "(.*desligar escritório.*)")
+    def st_office_off(self, speech, language):
+        self.say("Desligando as luzes do escritório.","Turning off office lights")
+        os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_Office__Turn_Light_OFF.jar'")
+        self.complete_request()
