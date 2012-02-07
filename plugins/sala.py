fb83c25ade0278c471850dd1e3eccbc08808e2db
Edson Alves Junior <edson.junior@me.com>
2012-02-07 21:53:35 -0200

    Fix code

--- plugins/sala.py
+++ plugins/sala.py
@@ -9,29 +9,29 @@ class salacontrol(Plugin):
     @register("pt-BR", ".*ligar.*sala.*")
     def st_kitchenon(self, speech, language):
         resposta = self.ask(u"Direta ou Indireta?")
-        if resposta == "direta"
+        if resposta == 'direta':
             self.say("Direta",null)
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_1.jar'")
-        if resposta == "indireta"
+        if resposta == 'indireta':
             self.say("Indireta",null)
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_3.jar'")
-        else self.say("Desculpe, não entendi.","Sorry, I didn't get that.")
+                else: self.say("Desculpe, não entendi.","Sorry, I didn't get that.")
         self.complete_request()
 
     @register("pt-BR", ".*desligar.*sala.*")
     def st_kitchenon(self, speech, language):
         resposta = self.ask(u"Direta ou Indireta?")
-        if resposta == "direta"
+        if resposta == 'direta':
             self.say("Direta",null)
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_1.jar'")
-        if resposta == "indireta"
+        if resposta == 'indireta':
             self.say("Indireta",null)
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_3.jar'")
-        else self.say("Desculpe, não entendi.","Sorry, I didn't get that.")
+            else: self.say("Desculpe, não entendi.","Sorry, I didn't get that.")
         self.complete_request()
