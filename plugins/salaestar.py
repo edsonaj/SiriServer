dc3dd72429c70b7035d6ff0ae68d3e0cba742c35
Edson Alves Junior <edson.junior@me.com>
2012-02-07 22:17:58 -0200

    Test

--- plugins/salaestar.py
+++ plugins/salaestar.py
@@ -10,16 +10,16 @@ class salacontrol(Plugin):
     def st_room_on(self, speech, language):
         resposta = self.ask(u"Direta ou Indireta?")
         if resposta == "Direta":
-        	self.say("{0}",null)
+        	self.say("{0}",none)
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_1.jar'")
         else if resposta == "Indireta":
-        	self.say("{0}",null)
+        	self.say("{0}",none)
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_3.jar'")
         else:
-        	self.say("{0}",null)
+        	self.say("{0}",none)
         	self.say("Desculpe, nao entendi.","Sorry, I didn't get that.")
     	self.complete_request()
 
@@ -27,15 +27,15 @@ class salacontrol(Plugin):
     def st_room_off(self, speech, language):
         resposta = self.ask(u"Direta ou Indireta?")
         if resposta == "Direta":
-        	self.say("{0}",null)
+        	self.say("{0}",none)
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_1.jar'")
         else if resposta == "Indireta":
-        	self.say("{0}",null)
+        	self.say("{0}",none)
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_3.jar'")
         else:
-        	self.say("{0}",null)
+        	self.say("{0}",none)
         	self.say("Desculpe, nao entendi.","Sorry, I didn't get that.")	
 	self.complete_request()
