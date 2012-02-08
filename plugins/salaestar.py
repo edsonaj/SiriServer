138692335d88f13bd5624d72c92897e3e3915345
Edson Alves Junior <edson.junior@me.com>
2012-02-07 22:35:42 -0200

    Revert b108a8602b8f7755bf47767bb7062f0b3f4ce479^..HEAD

--- plugins/salaestar.py
+++ plugins/salaestar.py
@@ -4,32 +4,36 @@
 import os
 from plugin import *
 
-class sala(Plugin):
+class salacontrol(Plugin):
     
     @register("pt-BR", ".*ligar.*sala.*")
-    def st_roomon(self, speech, language):
-        resp1 = self.ask("Direta ou Indireta?")
-        if resp1 == "Direta":
+    def st_room_on(self, speech, language):
+        resposta = self.ask(u"Direta ou Indireta?")
+        if resposta == "direta":
+            self.say("Direta",null)
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_1.jar'")
-        else if resp1 == "Indireta":
+        if resposta == "indireta":
+            self.say("Indireta",null)
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_3.jar'")
-        else:
+        else: 
         	self.say("Desculpe, nao entendi.","Sorry, I didn't get that.")
     	self.complete_request()
 
     @register("pt-BR", ".*desligar.*sala.*")
-    def st_roomoff(self, speech, language):
-        resp2 = self.ask("Direta ou Indireta?")
-        if resp2 == "Direta":
+    def st_room_off(self, speech, language):
+        resposta = self.ask(u"Direta ou Indireta?")
+        if resposta == "direta":
+            self.say("Direta",null)
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_1.jar'")
-        else if resp2 == "Indireta":
+        if resposta == "indireta":
+            self.say("Indireta",null)
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_3.jar'")
-        else:
+        else: 
         	self.say("Desculpe, nao entendi.","Sorry, I didn't get that.")	
 	self.complete_request()
