d7393d485906c95f87922a414fd024cd848d5bf9
Edson Alves Junior <edson.junior@me.com>
2012-02-07 22:32:26 -0200

    Fix

--- plugins/salaestar.py
+++ plugins/salaestar.py
@@ -4,15 +4,15 @@
 import os
 from plugin import *
 
-class salacontrol(Plugin):
+class sala(Plugin):
     
     @register("pt-BR", ".*ligar.*sala.*")
-    def st_room_on(self, speech, language):
-        resposta = self.ask(u"Direta ou Indireta?")
-        if resposta == "Direta":
+    def st_roomon(self, speech, language):
+        resp1 = self.ask("Direta ou Indireta?")
+        if resp1 == "Direta":
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_1.jar'")
-        else if resposta == "Indireta":
+        else if resp1 == "Indireta":
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_3.jar'")
@@ -21,12 +21,12 @@ class salacontrol(Plugin):
     	self.complete_request()
 
     @register("pt-BR", ".*desligar.*sala.*")
-    def st_room_off(self, speech, language):
-        resposta = self.ask(u"Direta ou Indireta?")
-        if resposta == "Direta":
+    def st_roomoff(self, speech, language):
+        resp2 = self.ask("Direta ou Indireta?")
+        if resp2 == "Direta":
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_1.jar'")
-        else if resposta == "Indireta":
+        else if resp2 == "Indireta":
             self.say("Desligando as luzes da sala.","Turning off living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_OFF_3.jar'")
