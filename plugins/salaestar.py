5f474e7b956d98fc2922bb0b1c205286aa322513
Edson Alves Junior <edson.junior@me.com>
2012-02-07 22:25:44 -0200

    Fix

--- plugins/salaestar.py
+++ plugins/salaestar.py
@@ -13,12 +13,10 @@ class salacontrol(Plugin):
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_1.jar'")
         else if resposta == "Indireta":
-        	self.say("{0}")
             self.say("Ligando as luzes da sala.","Turning on living room lights") 
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_2.jar'")
             os.system("java -jar '/home/ubuntu/SiriProxy/KNX/_LivingRoom__Turn_Light_ON_3.jar'")
         else:
-        	self.say("{0}")
         	self.say("Desculpe, nao entendi.","Sorry, I didn't get that.")
     	self.complete_request()
 
